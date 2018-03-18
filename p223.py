d4002077c45e91e1e069868028dd411381bc0b9d
wz980305 <shiweizhe@outlook.com>
2018-03-18 12:16:06 +0800

    little

---
+++ p223.py
@@ -0,0 +1,8 @@
+song_lyrics_str ='''When you're chewing on life's gristle
+Don't grumble, give a wristle
+And this'll help things turn out for the best...
+
+And...always look on the bright side of life...
+Always look on the light side of life...'''
+
+print(song_lyrics_str)
